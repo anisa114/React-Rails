9b04ee5f58fc2ba6dcbd241d0258c9a64ac12ddf
Anisa <anisa.tahlil22@gmail.com>
2019-05-15 02:34:59 -0400

    fixed seeding file with proper questions

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -19,13 +19,13 @@ survey2 = Survey.create(name:"Arthritis");
 # Creating Questions for Survey 1
 question1 = Question.create(survey_id:1, text:"How long has it been since your last epileptic seizure?")
 question2 = Question.create(survey_id:1, text:"How many seizures have you experienced during the past 4 weeks?")
-question3 = Question.create(survey_id:1, text:"How many seizures have you experienced during the past 4 weeks?")
-question4 = Question.create(survey_id:1, text:"How severe are you seizres?")
+question3 = Question.create(survey_id:1, text:"How severe are your seizures ?")
+question4 = Question.create(survey_id:1, text:"When your attacks happen, do you experience or feel an aura or a warning with your attack?")
 question5 = Question.create(survey_id:1, text:"What medication do you take and how often do you take it?")
 
 
 # Creating Questions for Survey 2
-question6 = Question.create(survey_id:2, text:"How long has it been since got diagnosed with Arthritis?")
+question6 = Question.create(survey_id:2, text:"How long has it been since you got diagnosed with Arthritis?")
 question7 = Question.create(survey_id:2, text:"Do you experience pain? How severe is it?")
 question8 = Question.create(survey_id:2, text:"How often do you visit the doctor?")
 question9 = Question.create(survey_id:2, text:"Does the arthritis impede you from completing daily tasks? Please give an example?")
